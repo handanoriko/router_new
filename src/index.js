6ac0443730d8769e9e0953396370a6d36d248737
handanoriko <106826874+handanoriko@users.noreply.github.com>
2022-06-26 22:02:48 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(<App />);
+
+// import React from "react";
+// import ReactDom from "react-dom";
+
+// import { App } from "./App";
+
+// ReactDom.render(<App />, document.getElementById("root"));
